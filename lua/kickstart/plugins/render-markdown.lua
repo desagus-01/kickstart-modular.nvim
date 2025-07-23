57a0d37e41b5fc4634d8ae35bb17b64c6a48392d
desagus <gugafera2016@outlook.com>
2025-07-23 11:50:26 +0100

    minor changes

--- lua/kickstart/plugins/render-markdown.lua
+++ lua/kickstart/plugins/render-markdown.lua
@@ -5,6 +5,12 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    completions = {
+      lsp = {
+        enabled = true,
+      },
+    },
+  },
   ft = { 'markdown', 'codecompanion' },
 }
