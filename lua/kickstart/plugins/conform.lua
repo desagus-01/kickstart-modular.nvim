d94396546b0215bf459a60d8e9f318dd1bb47d43
desagus <gugafera2016@outlook.com>
2025-07-29 09:19:16 +0100

    updating conform for auto linting

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,6 +31,10 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        hcl = { 'packer_fmt' },
+        terraform = { 'terraform_fmt' },
+        tf = { 'terraform_fmt' },
+        ['terraform_vars'] = { 'terraform_fmt' },
         -- Conform can also run multiple formatters sequentially
         python = { 'ruff_format', 'ruff_organize_imports' },
         --
