5f617469cbe0bc897082b498980c39f9f82863dd
desagus <gugafera2016@outlook.com>
2025-07-31 09:57:01 +0100

    minor addition to images in md

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -27,16 +27,9 @@ return {
     image = {
       enabled = true,
       doc = {
-        enabled = true,
-        inline = true, -- inline if terminal supports placeholders
+        -- enabled = true,
+        inline = false, -- inline if terminal supports placeholders
         float = true, -- fallback float when placeholders are not available
-        max_width = 80,
-        max_height = 40,
-
-        -- Hide the raw markdown link while the picture is visible
-        conceal = function(lang, type)
-          return type == 'image' -- still show $$math$$ blocks
-        end,
       },
     },
     input = { enabled = true },
