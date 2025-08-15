a2a5dfcb05d8d9221a9c0a519eb4de89e27cd467
Gustavo de Sa <gugafera2016@outlook.com>
2025-08-15 21:56:26 +0100

    small addition

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
+    bigfile = { enabled = true },
     dashboard = {
       enabled = true,
       formats = {
@@ -11,11 +12,11 @@ return {
           return { { '[', hl = 'special' }, { item.key, hl = 'key' }, { ']', hl = 'special' } }
         end,
       },
-      sections = { -- Dashboard sections & layout
-        { section = 'tejminal', cmd = 'fortune -s | cowsay', hl = 'header', padding = 3, indent = 8 },
-        { title = 'Bookiarks', padding = 1 },
+      sections = {
+        { section = 'terminal', cmd = 'fortune -s | cowsay', hl = 'header', padding = 3, indent = 8 },
+        { title = 'Bookmarks', padding = 1 },
         { section = 'keys', padding = 1 },
-        { title = 'Reclnt Files ', file = vim.fn.fnamemodify('.', ':~'), padding = 1 },
+        { title = 'Recent Files ', file = vim.fn.fnamemodify('.', ':~'), padding = 1 },
         { section = 'recent_files', cwd = true, limit = 8, padding = 1 },
         { title = 'Sessions', padding = 1 },
         { section = 'projects', padding = 1 },
@@ -23,28 +24,25 @@ return {
         { section = 'terminal', cmd = 'git --no-pager diff --stat -B -M -C', padding = 1 },
       },
     },
-
-    image = { -- Display images inside Neovim (with supported terminal)
+    image = {
       enabled = true,
       doc = {
         -- enabled = true,
-        inline = false,
-        float = true,
+        inline = false, -- inline if terminal supports placeholders
+        float = true, -- fallback float when placeholders are not available
       },
     },
-
-    -- input = { enabled = true }, -- Styled `vim.ui.input()` prompt
-    terminal = { enabled = true }, -- Floating terminal integration
-    picker = { enabled = true }, -- Fuzzy-finder UI for files, commands, etc.
-    notifier = { enabled = true }, -- Non-blocking notifications
-    lazygit = { enabled = true }, -- Run Lazygit inside Neovim
-    quickfile = { enabled = true }, -- Auto-close temporary files
-    -- scope = { enabled = true }, -- Highlight active buffer/window scope
-    scroll = { enabled = true }, -- Smooth scrolling animations
-    -- statuscolumn = { enabled = true }, -- Custom status column for signs/line numbers
-    rename = { enabled = true }, -- File rename helper
-    words = { enabled = true }, -- Highlight other occurrences of current word
-    indent = { enabled = true }, -- Show indentation guides
-    bigfile = { enabled = true },
+    input = { enabled = true },
+    terminal = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    lazygit = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    rename = { enabled = true },
+    words = { enabled = true },
+    indent = { enabled = true },
   },
 }
