22a62e25d43f9121365a9bac31dfcb1fd999181a
Gustavo de Sa <gugafera2016@outlook.com>
2025-08-19 19:22:27 +0100

    updating codecompanion to also work on laptop

--- lua/kickstart/plugins/code-companion.lua
+++ lua/kickstart/plugins/code-companion.lua
@@ -1,31 +1,51 @@
 return {
   'olimorris/codecompanion.nvim',
-  opts = {
-    provider = 'telescope',
-    strategies = {
-      chat = { adapter = 'qwen3' },
-      inline = { adapter = 'qwen3' },
-      cmd = { adapter = 'qwen3' },
-    },
-    adapters = {
-      qwen3 = function()
-        return require('codecompanion.adapters').extend('ollama', {
-          name = 'qwen2.5-coder (local)',
-          opts = {
-            vision = false,
-            stream = true,
-          },
-          schema = {
-            model = {
-              -- 2) Must match `ollama list`
-              default = 'qwen3:14b',
+  opts = function()
+    local function get_ollama_model()
+      return vim.env.OLLAMA_MODEL
+    end
+
+    if pcall(get_ollama_model) then
+      Chosen = get_ollama_model()
+    else
+      vim.notify('No OLLAMA_MODEL ENV found, setting default model qwen3:8b', vim.log.levels.error)
+      Chosen = 'qwen3:8b'
+    end
+
+    -- set special options if we're on qwen3:8b
+    local special_opts = { stream = true, vision = false }
+    if Chosen == 'qwen3:8b' then
+      special_opts = {
+        stream = true,
+        vision = false,
+        think = { default = false },
+        -- ollama extra params for efficiency on mid range CPU (laptop)
+        threads = 4,
+        batch = 128,
+        ctx = 4096,
+      }
+    end
+
+    return {
+      provider = 'telescope',
+      strategies = {
+        chat = { adapter = 'qwen_coder' },
+        inline = { adapter = 'qwen_coder' },
+        cmd = { adapter = 'qwen_coder' },
+      },
+      adapters = {
+        qwen_coder = function()
+          return require('codecompanion.adapters').extend('ollama', {
+            name = 'Qwen Coder (Ollama)',
+            opts = special_opts,
+            schema = {
+              model = {
+                default = Chosen,
+              },
             },
-            -- optional but nice:
-            keep_alive = { default = '5m' },
-          },
-        })
-      end,
-    },
-  },
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
+          })
+        end,
+      },
+    }
+  end,
 }
