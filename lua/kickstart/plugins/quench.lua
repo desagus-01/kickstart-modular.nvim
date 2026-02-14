60c098bcac94092cc0fa24541403ac13667011f2
Gustavo de Sa <gugafera2016@outlook.com>
2026-02-14 12:47:50 +0000

    minor changes to quench

--- lua/kickstart/plugins/quench.lua
+++ lua/kickstart/plugins/quench.lua
@@ -1,11 +1,14 @@
 return {
   'ryan-ressmeyer/quench.nvim',
   ft = 'python',
-  config = function()
+
+  init = function()
     vim.g.quench_nvim_web_server_host = '127.0.0.1'
     vim.g.quench_nvim_web_server_port = 8765
     vim.g.quench_nvim_autostart_server = false
+  end,
 
+  config = function()
     local group = vim.api.nvim_create_augroup('QuenchPythonKeys', { clear = true })
 
     vim.api.nvim_create_autocmd('FileType', {
@@ -20,10 +23,11 @@ return {
             desc = desc,
           })
         end
+
         map('n', '<leader>pq', '<cmd>QuenchResetKernel<CR>', 'Reset Kernel')
         map('n', '<leader>pk', '<cmd>QuenchSelectKernel<CR>', 'Select kernel')
         map('n', '<leader>pa', '<cmd>QuenchRunAll<CR>', 'Run All Cells')
-        map('n', '<leader>pb', '<cmd>QuenchRunAboveCR>', 'Run Cells below')
+        map('n', '<leader>pb', '<cmd>QuenchRunBelow<CR>', 'Run Cells below') -- fixed
         map('n', '<leader>pc', function()
           vim.api.nvim_put({ '# %%' }, 'l', true, true)
         end, 'Insert cell marker')
